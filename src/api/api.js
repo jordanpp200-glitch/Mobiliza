6d5ffa0a5b0f41cecae204aeab0560145893b38d
Jordan <jordanp.p200@gmail.com>
2026-07-07 18:45:07 -0300

    Atualiza URL da API para produção

--- src/api/api.js
+++ src/api/api.js
@@ -1,29 +1,20 @@
 import axios from "axios";
 
 const api = axios.create({
-
-    baseURL: "http://localhost:3000",
-
-    headers: {
-
-        "Content-Type": "application/json"
-
-    }
-
+  baseURL: "https://api-mobiliza.onrender.com",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
-api.interceptors.request.use((config)=>{
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
 
-    const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
 
-    if(token){
-
-        config.headers.Authorization = `Bearer ${token}`;
-
-    }
-
-    return config;
-
+  return config;
 });
 
 export default api;
